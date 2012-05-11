f8c53bc3fb2066a34175878cba972c9345d1fb8f
Gabriel Quiroz <gvquiroz@gmail.com>
2012-05-11 15:34:44 -0300

    TP1 Lenguajes

---
+++ MarioBros.java
@@ -0,0 +1,20 @@
+package ar.edu.untref.lp1.plomeria.grupoN;
+
+import ar.edu.untref.lp1.plomeria.Material;
+import ar.edu.untref.lp1.plomeria.Plomero;
+
+/**
+ * Ejemplo de implemetación de la interfaz Plomero.
+ * 
+ * @author Mariano Tugnarelli
+ *
+ */
+public class MarioBros implements Plomero {
+
+    @Override
+    public void construirCañeria(Material[][] pared) {
+
+        // Implementación
+    }
+
+}
