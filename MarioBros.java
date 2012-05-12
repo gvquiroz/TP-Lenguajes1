c4efe8f7bef47e7416591939deca9dd2b90303ab
gNotebook <gvquiroz@gmail.com>
2012-05-12 15:34:54 -0300

    Dia1 TP

--- MarioBros.java
+++ MarioBros.java
@@ -1,16 +1,43 @@
 
-/**
- * Ejemplo de implemetación de la interfaz Plomero.
- * 
- * @author Mariano Tugnarelli
- *
- */
+import grupoN.Plomero;
+import grupoN.Material;
+
+	
 public class MarioBros implements Plomero {
-
-    @Override
+	
+    public int filaDelSegundoCaño= 0;
+	public int filaDelPrimerCaño= 0;
+	public int columnaDelPrimerCaño= 0;
+	public int columnaSegundoCaño= 0;
+	public Material[][] miPared;
+	@Override
     public void construirCañeria(Material[][] pared) {
+		this.miPared = pared;
+    	buscandoCaños(pared);
+    	this.miPared[0][1] = Material.CAÑO_ABAJO_IZQUIERDA;
+    	for (int i = 1; i < this.filaDelSegundoCaño-1; i++){
+    		this.miPared[1][i] = Material.CAÑO_VERTICAL;
+    	}
+    	this.miPared[this.filaDelSegundoCaño][1] = Material.CAÑO_ARRIBA_DERECHA;
+    	
+    	
+	}
 
-        // Implementación
-    }
+	private void buscandoCaños(Material[][] pared) {
+		for (int i = 0;i < pared[i].length; i++){
+    		if (pared[0][i] == Material.CAÑO_HORIZONTAL ){
+    			this.filaDelPrimerCaño = i;
+    			this.columnaDelPrimerCaño = 0;
+    		}
+    	}
+    	for (int i = 0; i < pared[i].length; i++){
+    		if (pared[pared.length-1][i] == Material.CAÑO_HORIZONTAL ){
+    			this.filaDelSegundoCaño = i;
+    			this.columnaSegundoCaño = pared.length-1;
+    		}
+    	}
+	}
 
 }
+
+
