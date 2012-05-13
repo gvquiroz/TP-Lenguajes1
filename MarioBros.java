640bc3df2352addd2c07561ac5c25322324eb61c
gNotebook <gvquiroz@gmail.com>
2012-05-13 11:53:29 -0300

    Algoritmo alpha1

--- MarioBros.java
+++ MarioBros.java
@@ -5,35 +5,100 @@ import grupoN.Material;
 	
 public class MarioBros implements Plomero {
 	
-    public int filaDelSegundoCaño= 0;
-	public int filaDelPrimerCaño= 0;
-	public int columnaDelPrimerCaño= 0;
-	public int columnaSegundoCaño= 0;
-	public Material[][] miPared;
+    public int filaDelSegundoCaño=0;
+	public int filaDelPrimerCaño=0;
+	public int columnaDelPrimerCaño=0;
+	public int columnaSegundoCaño=0;
+	public Material[][] nuevaPared;
 	@Override
+	/**
+	 * funcionamiento: si el ultimo caño no esta en la ultima columna del array se sigue ejecutando
+	 * 
+	 */
     public void construirCañeria(Material[][] pared) {
-		this.miPared = pared;
-    	buscandoCaños(pared);
-    	this.miPared[0][1] = Material.CAÑO_ABAJO_IZQUIERDA;
-    	for (int i = 1; i < this.filaDelSegundoCaño-1; i++){
-    		this.miPared[1][i] = Material.CAÑO_VERTICAL;
-    	}
-    	this.miPared[this.filaDelSegundoCaño][1] = Material.CAÑO_ARRIBA_DERECHA;
-    	
+		this.nuevaPared = pared;
+		while (this.columnaSegundoCaño != this.nuevaPared.length-1){
+			buscandoCaños(pared);
+			primerCañoArribaDelSegundoCaño();
+			primerCañoDebajoDelSegundoCaño();
+			primerCañoAlMismoNivelQueElSegundo();
+		}
     	
 	}
-
+/**
+ * pre: si los caños estan al mismo nivel en filas
+ * post: une el primer caño con el segundo con caños horizontales
+ */
+	private void primerCañoAlMismoNivelQueElSegundo() {
+		if(this.filaDelPrimerCaño == this.filaDelSegundoCaño){
+			for (int i = this.columnaDelPrimerCaño+1; i < this.columnaSegundoCaño;i++){
+				this.nuevaPared[i][this.filaDelPrimerCaño] = Material.CAÑO_HORIZONTAL;
+			}
+		}
+		
+	}
+/**
+ * pre: si el primer caño esta debajo del segundo
+ * post: une el priemr caño con el segundo usando Material.CAÑO_ARRIBA_IZQUIERDA y Material.CAÑO_ABAJO_DERECHA,
+ *  en el caso que sea necesario unira estos 2 con caños_verticales.
+ */
+	private void primerCañoDebajoDelSegundoCaño() {
+		if (this.filaDelPrimerCaño > this.filaDelSegundoCaño){
+			this.nuevaPared[this.columnaDelPrimerCaño+1][this.filaDelPrimerCaño] = Material.CAÑO_ARRIBA_IZQUIERDA;
+			for (int i = this.filaDelPrimerCaño -1; i > this.filaDelSegundoCaño;i--){
+				this.nuevaPared[this.columnaDelPrimerCaño+1][i] = Material.CAÑO_VERTICAL;
+			}
+			this.nuevaPared[this.columnaDelPrimerCaño+1][this.filaDelSegundoCaño] = Material.CAÑO_ABAJO_DERECHA;
+			for (int i = this.columnaDelPrimerCaño+2; i < this.columnaSegundoCaño;i++){
+				this.nuevaPared[i][this.filaDelSegundoCaño] = Material.CAÑO_HORIZONTAL;
+			}
+		}
+	}
+/**
+ * pre: si el primer caño esta arriba del segundo
+ * post:une el primer caño con el segundo usando Material.CAÑO_ABAJO_IZQUIERDA y Material.CAÑO_ARRIBA_DERECHA
+ * en el caso que sea necesario unira estos 2 con caños_verticales.
+ */
+	private void primerCañoArribaDelSegundoCaño() {
+		if (this.filaDelPrimerCaño < this.filaDelSegundoCaño){
+			this.nuevaPared[this.columnaDelPrimerCaño+1][this.filaDelPrimerCaño] = Material.CAÑO_ABAJO_IZQUIERDA;
+			for (int i = this.filaDelPrimerCaño +1; i < this.filaDelSegundoCaño; i++){
+				this.nuevaPared[this.columnaDelPrimerCaño +1][i] = Material.CAÑO_VERTICAL;
+			}
+			this.nuevaPared[this.columnaDelPrimerCaño+1][this.filaDelSegundoCaño] = Material.CAÑO_ARRIBA_DERECHA;
+			for (int i = this.columnaDelPrimerCaño+2; i < this.columnaSegundoCaño; i++){
+				this.nuevaPared[i][this.filaDelSegundoCaño] = Material.CAÑO_HORIZONTAL;
+			}
+		}	
+	}
+/**
+ * Busca un primer caño horizontal empezando desde la columna posterior al caño encontrado 
+ * (en el caso de que no hubiesen caños anteriormente empieza de la columna 0)
+ * guarda la fila y la columna del primer caño encontrado
+ * busca un segundo caño horizontal empezando desde la columan posterior al caño encontrado
+ * guarda la fila y la columna del segundo caño encontrado
+ * @param pared
+ */
 	private void buscandoCaños(Material[][] pared) {
-		for (int i = 0;i < pared[i].length; i++){
-    		if (pared[0][i] == Material.CAÑO_HORIZONTAL ){
-    			this.filaDelPrimerCaño = i;
-    			this.columnaDelPrimerCaño = 0;
+		boolean primerCañoEncontrado = false;
+		for (int i = this.columnaSegundoCaño;i < pared.length && !primerCañoEncontrado; i++){
+			for(int j = 0;j < pared[i].length; j++){
+				if (pared[i][j] == Material.CAÑO_HORIZONTAL ){
+					this.filaDelPrimerCaño = j;
+					this.columnaDelPrimerCaño = i;
+					primerCañoEncontrado = true;
+    			}
     		}
     	}
-    	for (int i = 0; i < pared[i].length; i++){
-    		if (pared[pared.length-1][i] == Material.CAÑO_HORIZONTAL ){
-    			this.filaDelSegundoCaño = i;
-    			this.columnaSegundoCaño = pared.length-1;
+		boolean segundoCañoEncontrado = false;
+    	for (int i = this.columnaDelPrimerCaño + 1; i < pared.length && !segundoCañoEncontrado; i++){
+    		for(int j = 0; j < pared[i].length; j++){
+    			if (pared[i][j] == Material.CAÑO_HORIZONTAL ){
+    				this.filaDelSegundoCaño = j;
+    				this.columnaSegundoCaño = i;
+    				segundoCañoEncontrado = true;
+    			}
+    			
     		}
     	}
 	}
--- TESTMarioBros.java
+++ TESTMarioBros.java
@@ -6,15 +6,73 @@ import org.junit.Test;
 
 public class TESTMarioBros {
 	@Test
-	public void creandoPared(){
+	public void probandoPrimerCañoArribaDelSegundo(){
 		MarioBros miPlomero = new MarioBros();
-		Material[][] miPared = paredTest(5,4);
+		Material[][] miPared = paredConPrimerCañoArribaDelSegundo(5,4);
 		miPlomero.construirCañeria(miPared);
-		System.out.println("Fila del primer caño es "+miPlomero.filaDelPrimerCaño );
-		System.out.println("Columna del primer caño es "+ miPlomero.columnaDelPrimerCaño );
-		System.out.println("Fila del segundo caño es " + miPlomero.filaDelSegundoCaño );
-		System.out.println("Columna del segundo caño es " + miPlomero.columnaSegundoCaño);
-		Assert.assertEquals("probando ladrillo en 1,1",Material.LADRILLO, miPlomero.miPared[2][2] );
+		Assert.assertEquals("fila del primer caño",0, miPlomero.filaDelPrimerCaño);
+		Assert.assertEquals("columna del primer caño",0, miPlomero.columnaDelPrimerCaño);
+		Assert.assertEquals("fila del segundo caño",3, miPlomero.filaDelSegundoCaño);
+		Assert.assertEquals("columna del segundo caño",4, miPlomero.columnaSegundoCaño);
+
+		Assert.assertEquals("probando ladrillo en 2,2",Material.LADRILLO, miPlomero.nuevaPared[2][2] );
+		Assert.assertEquals("probando caño vertical en 1,2",Material.CAÑO_VERTICAL, miPlomero.nuevaPared[1][2] );
+		Assert.assertEquals("probando arriba derecha en 1,3",Material.CAÑO_ARRIBA_DERECHA, miPlomero.nuevaPared[1][3] );
+		Assert.assertEquals("probando horizontal 2,3",Material.CAÑO_HORIZONTAL, miPlomero.nuevaPared[2][3] );
+		Assert.assertEquals("probando horizontal en 3,3",Material.CAÑO_HORIZONTAL, miPlomero.nuevaPared[3][3] );
+		Assert.assertEquals("probando horizontal 4,3",Material.CAÑO_HORIZONTAL, miPlomero.nuevaPared[4][3] );
+	}
+	@Test
+	public void probandoPrimerCañoAbajoDelSegundo(){
+		MarioBros miPlomero2 = new MarioBros();
+		Material[][] miPared = paredConPrimerCañoAbajoDelSegundo(5,4);
+		miPlomero2.construirCañeria(miPared);
+		Assert.assertEquals("fila del primer caño",3, miPlomero2.filaDelPrimerCaño);
+		Assert.assertEquals("columna del primer caño",0, miPlomero2.columnaDelPrimerCaño);
+		Assert.assertEquals("fila del segundo caño",0, miPlomero2.filaDelSegundoCaño);
+		Assert.assertEquals("columna del segundo caño",4, miPlomero2.columnaSegundoCaño);
+		
+
+		Assert.assertEquals("probando Arriba izc en 1,3",Material.CAÑO_ARRIBA_IZQUIERDA, miPlomero2.nuevaPared[1][3] );
+		Assert.assertEquals("probando Vertical en 1,2",Material.CAÑO_VERTICAL, miPlomero2.nuevaPared[1][2] );
+		Assert.assertEquals("probando Vertical en 1,1",Material.CAÑO_VERTICAL, miPlomero2.nuevaPared[1][1] );
+		Assert.assertEquals("probando Abajo derecha en 1,0",Material.CAÑO_ABAJO_DERECHA, miPlomero2.nuevaPared[1][0] );
+		Assert.assertEquals("probando Horizontal en 2,0",Material.CAÑO_HORIZONTAL, miPlomero2.nuevaPared[2][0] );
+		Assert.assertEquals("probando Horizontal en 3,0",Material.CAÑO_HORIZONTAL, miPlomero2.nuevaPared[3][0] );
+
+	}
+	@Test
+	public void probandoPrimerCañoAlMismoNivelQueElSegundo(){
+		MarioBros miPlomero3 = new MarioBros();
+		Material[][] miPared = paredConPrimerCañoAIgualNivelQueElSegundo(5, 4);
+		miPlomero3.construirCañeria(miPared);
+		Assert.assertEquals("fila del primer caño",3, miPlomero3.filaDelPrimerCaño);
+		Assert.assertEquals("columna del primer caño",0, miPlomero3.columnaDelPrimerCaño);
+		Assert.assertEquals("fila del segundo caño",3, miPlomero3.filaDelSegundoCaño);
+		Assert.assertEquals("columna del segundo caño",4, miPlomero3.columnaSegundoCaño);
+		
+		Assert.assertEquals("probando Horizontal en 1,3",Material.CAÑO_HORIZONTAL, miPlomero3.nuevaPared[1][3] );
+		Assert.assertEquals("probando Horizontal en 2,3",Material.CAÑO_HORIZONTAL, miPlomero3.nuevaPared[2][3] );
+		Assert.assertEquals("probando Horizontal en 3,3",Material.CAÑO_HORIZONTAL, miPlomero3.nuevaPared[3][3] );
+		Assert.assertEquals("probando Horizontal en 4,3",Material.CAÑO_HORIZONTAL, miPlomero3.nuevaPared[4][3] );
+
+	}
+	@Test
+	public void probandoAlgoritmoConCañoHorizontalEnElMedio(){
+		MarioBros miPlomero4 = new MarioBros();
+		Material[][] miPared = probandoWhileCAMBIARRRRRR(7, 4);
+		miPlomero4.construirCañeria(miPared);
+		
+		Assert.assertEquals("probando Arriba izc en 1,3",Material.CAÑO_ARRIBA_IZQUIERDA, miPlomero4.nuevaPared[1][3] );
+		Assert.assertEquals("probando Vertical en 1,2",Material.CAÑO_VERTICAL, miPlomero4.nuevaPared[1][2] );
+		Assert.assertEquals("probando Abajo derecha en 1,1",Material.CAÑO_ABAJO_DERECHA, miPlomero4.nuevaPared[1][1] );
+		Assert.assertEquals("probando Horizontal en 2,1",Material.CAÑO_HORIZONTAL, miPlomero4.nuevaPared[2][1] );
+		Assert.assertEquals("probando Arriba izquierda en 3,1",Material.CAÑO_ARRIBA_IZQUIERDA, miPlomero4.nuevaPared[3][1] );
+		Assert.assertEquals("probando Abajo derecha en 3,0",Material.CAÑO_ABAJO_DERECHA, miPlomero4.nuevaPared[3][0] );
+		Assert.assertEquals("probando Horizontal en 4,0",Material.CAÑO_HORIZONTAL, miPlomero4.nuevaPared[4][0] );
+		Assert.assertEquals("probando Horizontal en 5,0",Material.CAÑO_HORIZONTAL, miPlomero4.nuevaPared[5][0] );
+		Assert.assertEquals("probando Horizontal en 6,0",Material.CAÑO_HORIZONTAL, miPlomero4.nuevaPared[6][0] );
+		
 	}
 	/**
 	 * 
@@ -23,10 +81,65 @@ public class TESTMarioBros {
 	 * @return
 	 * Crea una pared con Un caño_horizontal arriba a la izc y abajo a la derecha.
 	 */
-	private Material[][] paredTest(int ancho, int alto) {
+	private Material[][] paredConPrimerCañoArribaDelSegundo(int ancho, int alto) {
 		Material[][] miPared = new Material[ancho][alto];
 		miPared[0][0] = Material.CAÑO_HORIZONTAL;
 		miPared[miPared.length-1][miPared[0].length-1] = Material.CAÑO_HORIZONTAL;
+		for (int i = 0; i < miPared.length; i++){
+			for (int j = 0; j < miPared[i].length; j++){
+				if (miPared[i][j] != Material.CAÑO_HORIZONTAL){
+					miPared[i][j] = Material.LADRILLO;
+				}
+			}
+		}
+		return miPared;
+	}
+	/**
+	 * 
+	 * @param ancho
+	 * @param alto
+	 * @return
+	 * Crea una pared con caño_horizontal en la primera columna y ultima fila,
+	 * 							   y otro en la ultima columna y ultima fila
+	 */
+	private Material[][] paredConPrimerCañoAIgualNivelQueElSegundo(int ancho, int alto) {
+		Material[][] miPared = new Material[ancho][alto];
+		miPared[0][miPared[0].length - 1] = Material.CAÑO_HORIZONTAL;
+		miPared[miPared.length-1][miPared[0].length-1] = Material.CAÑO_HORIZONTAL;
+		for (int i = 0; i < miPared.length; i++){
+			for (int j = 0; j < miPared[i].length; j++){
+				if (miPared[i][j] != Material.CAÑO_HORIZONTAL){
+					miPared[i][j] = Material.LADRILLO;
+				}
+			}
+		}
+		return miPared;
+	}
+	/**
+	 * 
+	 * @param ancho
+	 * @param alto
+	 * @return
+	 * crea una pared con caño_horizontal en la primera columna ultima fila y otro en la ultima columna primera fila;
+	 */
+	private Material[][] paredConPrimerCañoAbajoDelSegundo(int ancho, int alto) {
+		Material[][] miPared = new Material[ancho][alto];
+		miPared[0][miPared[0].length - 1] = Material.CAÑO_HORIZONTAL;
+		miPared[miPared.length-1][0] = Material.CAÑO_HORIZONTAL;
+		for (int i = 0; i < miPared.length; i++){
+			for (int j = 0; j < miPared[i].length; j++){
+				if (miPared[i][j] != Material.CAÑO_HORIZONTAL){
+					miPared[i][j] = Material.LADRILLO;
+				}
+			}
+		}
+		return miPared;
+	}
+	private Material[][] probandoWhileCAMBIARRRRRR(int ancho, int alto) {
+		Material[][] miPared = new Material[ancho][alto];
+		miPared[0][miPared[0].length - 1] = Material.CAÑO_HORIZONTAL;
+		miPared[miPared.length-1][0] = Material.CAÑO_HORIZONTAL;
+		miPared[2][1] = Material.CAÑO_HORIZONTAL;
 		for (int i = 0; i < miPared.length; i++){
 			for (int j = 0; j < miPared[i].length; j++){
 				if (miPared[i][j] != Material.CAÑO_HORIZONTAL){
